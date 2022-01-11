ce069af4104d0c7d7c41d46e9c5a0da81f684b0b
Stanimir-P <stanimirpavlov0@gmail.com>
2022-01-11 17:09:06 +0200

    firebase database created

--- src/firebaseSetup.ts
+++ src/firebaseSetup.ts
@@ -3,13 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+	apiKey: "AIzaSyDgpwSaCHmgdFDBX7e4fnDHQKuToF-JHOQ",
+	authDomain: "pixelpool-49f3d.firebaseapp.com",
+	projectId: "pixelpool-49f3d",
+	storageBucket: "pixelpool-49f3d.appspot.com",
+	messagingSenderId: "950879132131",
+	appId: "1:950879132131:web:7a0a1a5ae552c7d58d28d6"
 };
 
 firebase.initializeApp(firebaseConfig);
