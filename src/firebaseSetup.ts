4bce4fd71c1869d1bab19c163784d529fd505bea
Stanimir-P <stanimirpavlov0@gmail.com>
2022-01-11 17:01:16 +0200

    initial commit

---
+++ src/firebaseSetup.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.firestore();
